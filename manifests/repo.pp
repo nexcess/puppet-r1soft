34446e801f5a92916ef02acb25a2a9477eb15264
Eric Cook <ecook@nexcess.net>
2015-09-01 14:54:13 -0400

    stub out the repo and wrote some tests.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,2 +1,9 @@
 class r1soft::repo {
+  yumrepo { 'r1soft':
+    ensure   => $r1soft::agent::repo_ensure,
+    enabled  => $r1soft::agent::repo_enabled,
+    gpgcheck => $r1soft::agent::repo_gpgcheck,
+    gpgkey   => $r1soft::agent::repo_gpgkey,
+    baseurl  => $r1soft::agent::repo_baseurl,
+  }
 }
--- spec/classes/agent_spec.rb
+++ spec/classes/agent_spec.rb
@@ -13,6 +13,18 @@ describe 'r1soft::agent' do
           it { should contain_class('r1soft::agent::install') }
           it { should contain_class('r1soft::agent::kernel_module') }
           it { should contain_class('r1soft::agent::service') }
+          
+          describe 'r1soft::repo' do
+            it { should contain_yumrepo('r1soft').with_enabled('1') }
+            it { should contain_yumrepo('r1soft').with_baseurl('http://repo.r1soft.com/yum/stable/x86_64/') }
+            it { should contain_yumrepo('r1soft').with_ensure('present') }
+            it { should contain_yumrepo('r1soft').with_gpgcheck('1') }
+            it { should contain_yumrepo('r1soft').with_gpgkey('https://www.ksplice.com/yum/RPM-GPG-KEY-ksplice') }
+          end
+
+          describe 'r1soft::agent::install' do
+            it { should contain_package('serverbackup-agent').with_cdp_agent_install('true') }
+          end
         end
       end
     end
