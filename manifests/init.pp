09e729e4e36cb26c66b81b75e3d7232d936abe6a
Mark McKinstry <mmckinst@nexcess.net>
2015-09-01 12:23:25 -0400

    explanation for why r1soft is empty

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,2 +1,6 @@
 class r1soft {
+  # Nothing is in here.
+  #
+  # To use the module, you want to include '::r1soft::agent' or
+  # '::r1soft::server'. Plain old '::r1soft' does nothing by itself.
 }
