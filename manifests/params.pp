dc1b13124d5f035f762ebf7b55c81253a25626f6
Eric Cook <ecook@nexcess.net>
2015-09-02 14:38:25 -0400

    Remove the ensure attribute from the yumrepo type in manifests/repo.pp
    For compatibility with puppet 2.7.23

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,6 @@
 class r1soft::params {
   $repo_install          = true
   $repo_baseurl          = 'http://repo.r1soft.com/yum/stable/x86_64/'
-  $repo_ensure           = 'present'
   $repo_enabled          = '1'
   $repo_gpgcheck         = '1'
   $repo_gpgkey           = 'https://www.ksplice.com/yum/RPM-GPG-KEY-ksplice'
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,13 +1,11 @@
 class r1soft::repo (
   $repo_install  = $r1soft::params::repo_install,
   $repo_baseurl  = $r1soft::params::repo_baseurl,
-  $repo_ensure   = $r1soft::params::repo_ensure,
   $repo_enabled  = $r1soft::params::repo_enabled,
   $repo_gpgcheck = $r1soft::params::repo_gpgcheck,
   $repo_gpgkey   = $r1soft::params::repo_gpgkey,
 ) inherits r1soft::params {
   yumrepo { 'r1soft':
-    ensure   => $repo_ensure,
     enabled  => $repo_enabled,
     gpgcheck => $repo_gpgcheck,
     gpgkey   => $repo_gpgkey,
