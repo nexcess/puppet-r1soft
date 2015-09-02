e2491f2ed9ae1fb3d40bba3338c27efd7db88db4
Eric Cook <ecook@nexcess.net>
2015-09-02 18:26:54 -0400

    Add the parameters $kernel_devel_install and $kernel_devel_package_names, allowing the user to override the kernel packages to be installed.

--- manifests/agent/kernel_package.pp
+++ manifests/agent/kernel_package.pp
@@ -1,5 +1,11 @@
-class r1soft::agent::kernel_package {
-  package { ["kernel-devel","kernel-devel-${kernelversion}"]:
-    ensure => $r1soft::agent::cdp_agent_version,
+class r1soft::agent::kernel_package(
+  $cdp_agent_version          = $r1soft::params::cdp_agent_version,
+  $kernel_devel_install       = $r1soft::params::kernel_devel_install,
+  $kernel_devel_package_names = $r1soft::params::kernel_devel_package_names,
+) inherits r1soft::params {
+  if $kernel_devel_install {
+    package { $kernel_devel_package_names:
+      ensure => $cdp_agent_version,
+    }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,15 @@
 class r1soft::params {
-  $repo_install          = true
-  $repo_baseurl          = 'http://repo.r1soft.com/yum/stable/x86_64/'
-  $repo_enabled          = '1'
-  $repo_gpgcheck         = '1'
-  $repo_gpgkey           = 'https://www.ksplice.com/yum/RPM-GPG-KEY-ksplice'
-  $cdp_agent_install     = true
-  $cdp_agent_version     = 'present'
-  $kernel_module_install = true
-  $kernel_module_name    = 'hcpdriver'
-  $service_manage        = true
-  $service_name          = 'cdp-agent'
+  $repo_install               = true
+  $repo_baseurl               = 'http://repo.r1soft.com/yum/stable/x86_64/'
+  $repo_enabled               = '1'
+  $repo_gpgcheck              = '1'
+  $repo_gpgkey                = 'https://www.ksplice.com/yum/RPM-GPG-KEY-ksplice'
+  $cdp_agent_install          = true
+  $cdp_agent_version          = 'present'
+  $kernel_devel_install       = true
+  $kernel_devel_package_names = ["kernel-devel","kernel-devel-${kernelversion}"]
+  $kernel_module_install      = true
+  $kernel_module_name         = 'hcpdriver'
+  $service_manage             = true
+  $service_name               = 'cdp-agent'
 }
