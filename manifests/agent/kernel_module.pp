4288b81cfaa8fd22c4dacb039b7f06ec94246c52
Eric Cook <ecook@nexcess.net>
2015-09-17 12:43:08 -0400

    use hcp-setup to retrieve kernel module.

--- manifests/agent/kernel_module.pp
+++ manifests/agent/kernel_module.pp
@@ -1,6 +1,6 @@
 class r1soft::agent::kernel_module {
   if $r1soft::agent::kernel_module_install {
-    exec { 'hcp-driver --get-module':
+    exec { 'hcp-setup --get-module':
        refreshonly => true,
        notify      => Package['serverbackup-agent'],
        path        => '/usr/sbin:/sbin:/bin:/usr/sbin',
