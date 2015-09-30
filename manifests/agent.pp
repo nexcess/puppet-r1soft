5e5e5d4d0c7d5fb81b125fc2d6e972cd65e3a104
Eric Cook <ecook@nexcess.net>
2015-09-30 13:00:55 -0400

    Ensure each class execute in the order we intended.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -3,9 +3,9 @@ class r1soft::agent (
   $cdp_agent_version = $r1soft::params::cdp_agent_version,
 ) inherits r1soft::params {
 
-  class{'::r1soft::repo':}
-  class{'::r1soft::agent::kernel_package':}
-  class{'::r1soft::agent::install':}
-  class{'::r1soft::agent::kernel_module':}
+  class{'::r1soft::repo':} ->
+  class{'::r1soft::agent::kernel_package':} ->
+  class{'::r1soft::agent::install':} ->
+  class{'::r1soft::agent::kernel_module':} ->
   class{'::r1soft::agent::service':}
 } 
--- manifests/agent/kernel_module.pp
+++ manifests/agent/kernel_module.pp
@@ -2,7 +2,6 @@ class r1soft::agent::kernel_module {
   if $r1soft::agent::kernel_module_install {
     exec { 'hcp-setup --get-module':
        refreshonly => true,
-       notify      => Package['serverbackup-agent'],
        path        => '/usr/sbin:/sbin:/bin:/usr/sbin',
     }
   }
