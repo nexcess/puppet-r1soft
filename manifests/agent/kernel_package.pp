600d7326a94dd398747fc214bf4a87159efe6f29
Eric Cook <ecook@nexcess.net>
2015-10-01 15:00:00 -0400

    remove $cdp_agent_version from agent/kernel_package.pp

--- manifests/agent/kernel_package.pp
+++ manifests/agent/kernel_package.pp
@@ -1,11 +1,10 @@
 class r1soft::agent::kernel_package(
-  $cdp_agent_version          = $r1soft::params::cdp_agent_version,
   $kernel_devel_install       = $r1soft::params::kernel_devel_install,
   $kernel_devel_package_names = $r1soft::params::kernel_devel_package_names,
 ) inherits r1soft::params {
   if $kernel_devel_install {
     package { $kernel_devel_package_names:
-      ensure => $cdp_agent_version,
+      ensure => 'present',
     }
   }
 }
