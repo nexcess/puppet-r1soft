c6e407b32e49406c760250d46bf7da4cd241d3f7
Eric Cook <ecook@nexcess.net>
2015-10-12 15:11:48 -0400

    allow the repo management via yumrepo to be optional

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -5,11 +5,13 @@ class r1soft::repo (
   $repo_gpgcheck = $r1soft::params::repo_gpgcheck,
   $repo_gpgkey   = $r1soft::params::repo_gpgkey,
 ) inherits r1soft::params {
-  yumrepo { 'r1soft':
-    descr    => 'r1soft',
-    enabled  => $repo_enabled,
-    gpgcheck => $repo_gpgcheck,
-    gpgkey   => $repo_gpgkey,
-    baseurl  => $repo_baseurl,
+  if $repo_install {
+    yumrepo { 'r1soft':
+      descr    => 'r1soft',
+      enabled  => $repo_enabled,
+      gpgcheck => $repo_gpgcheck,
+      gpgkey   => $repo_gpgkey,
+      baseurl  => $repo_baseurl,
+    }
   }
 }
