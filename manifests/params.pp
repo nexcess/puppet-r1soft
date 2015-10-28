228d848709f8eaee8a5813495cdf41e1ee59d9c3
Mark McKinstry <mmckinst@nexcess.net>
2015-10-28 16:04:23 -0400

    use booleans and convert to numbers (easier to validate)

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class r1soft::params {
   $repo_baseurl               = 'http://repo.r1soft.com/yum/stable/$basearch/'
-  $repo_enabled               = '1'
-  $repo_gpgcheck              = '0' # r1soft doesn't sign their packages
+  $repo_enabled               = true
+  $repo_gpgcheck              = false # r1soft doesn't sign their packages
   $cdp_agent_package_version  = 'present'
   $cdp_agent_package_name     = 'serverbackup-agent'
   $kernel_devel_install       = true
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,8 +1,8 @@
 class r1soft::repo {
   yumrepo { 'r1soft':
     descr    => 'r1soft',
-    enabled  => $r1soft::agent::repo_enabled,
-    gpgcheck => $r1soft::agent::repo_gpgcheck,
+    enabled  => bool2num($r1soft::agent::repo_enabled),
+    gpgcheck => bool2num($r1soft::agent::repo_gpgcheck),
     baseurl  => $r1soft::agent::repo_baseurl,
   }
 }
