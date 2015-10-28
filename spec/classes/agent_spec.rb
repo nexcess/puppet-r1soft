f300edc9381366e46c231d752e30d7af752651d6
Mark McKinstry <mmckinst@nexcess.net>
2015-10-28 16:01:59 -0400

    test all the custom parameters

--- spec/classes/agent_spec.rb
+++ spec/classes/agent_spec.rb
@@ -13,7 +13,7 @@ describe 'r1soft::agent' do
           it { should contain_class('r1soft::agent::install') }
           it { should contain_class('r1soft::agent::kernel_module') }
           it { should contain_class('r1soft::agent::service') }
-          
+
           describe 'r1soft::repo' do
             it { should contain_yumrepo('r1soft').with_enabled('1') }
             it { should contain_yumrepo('r1soft').with_baseurl('http://repo.r1soft.com/yum/stable/$basearch/') }
@@ -38,6 +38,49 @@ describe 'r1soft::agent' do
             it { should contain_service('cdp-agent').with_ensure('running') }
             it { should contain_service('cdp-agent').with_enable('true') }
           end
+        end
+        context 'custom parameters' do
+
+          describe 'r1soft::repo allow custom basurl' do
+            let(:params) { {:repo_baseurl => 'http://example.com/yum/' } }
+            it { should contain_yumrepo('r1soft').with_baseurl('http://example.com/yum/') }
+          end
+          describe 'r1soft::repo allow custom enabled' do
+            let(:params) { {:repo_enabled => '0' } }
+            it { should contain_yumrepo('r1soft').with_enabled('0') }
+          end
+          describe 'r1soft::repo allow custom gpgcheck' do
+            let(:params) { {:repo_gpgcheck => '1' } }
+            it { should contain_yumrepo('r1soft').with_gpgcheck('1') }
+          end
+
+          describe 'r1soft::agent::install allow custom ensure/version' do
+            let(:params) { {:cdp_agent_package_version => '1.0.0' } }
+            it { should contain_package('serverbackup-agent').with_ensure('1.0.0') }
+          end
+          describe 'r1soft::agent::install allow custom name' do
+            let(:params) { {:cdp_agent_package_name => 'r1soft-agent' } }
+            it { should contain_package('r1soft-agent') }
+          end
+
+          describe 'r1soft::agent::kernel_package allow custom packagename' do
+            let(:params) { {:kernel_devel_package_names => 'custom-kernel-devel' } }
+            it { should contain_package('custom-kernel-devel') }
+          end
+
+          describe 'r1soft::agent::service with custom service name' do
+            let(:params) { {:service_name => 'backup-agent' } }
+            it { should contain_service('backup-agent') }
+          end
+          describe 'r1soft::agent::service with custom ensure' do
+            let(:params) { {:service_ensure => 'stopped' } }
+            it { should contain_service('cdp-agent').with_ensure('stopped') }
+          end
+          describe 'r1soft::agent::service with custom ensure' do
+            let(:params) { {:service_enable => false } }
+            it { should contain_service('cdp-agent').with_enable(false) }
+          end
+
         end
       end
     end
