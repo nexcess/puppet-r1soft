002e632cb08f6b1cc9760a32d19b060d49b01363
Mark McKinstry <mmckinst@nexcess.net>
2015-10-29 14:09:10 -0400

    facts for r1soft version

---
+++ lib/facter/r1soft.rb
@@ -0,0 +1,23 @@
+if File::exist?('/usr/sbin/r1soft/bin/cdp') then
+
+  r1soft_version_output = Facter::Util::Resolution.exec('/usr/sbin/r1soft/bin/cdp --version')
+
+  Facter.add(:r1soft_agent_version_short) do
+    setcode do
+      r1soft_version_output.split[4]
+    end
+  end
+
+  Facter.add(:r1soft_agent_version) do
+    setcode do
+      r1soft_version_output.split[4..6].join(' ')
+    end
+  end
+
+  Facter.add(:r1soft_agent_version_long) do
+    setcode do
+      r1soft_version_output.split[4..-1].join(' ')
+    end
+  end
+
+end
