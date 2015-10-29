933e03ea5055b1ef4157e92dd46ad2e080bd2a81
Mark McKinstry <mmckinst@nexcess.net>
2015-10-29 10:23:47 -0400

    more acceptance test for r1soft::agent and keys

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper_acceptance'
 
 describe 'Applying the r1soft::agent class' do
-  describe 'running puppet code' do
+  describe 'running r1soft::agent with no parameters' do
     it 'should work with no errors' do
       pp = <<-EOS
-         class { 'r1soft::agent': }
+         class {'::r1soft::agent':}
       EOS
 
       # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
       expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
     end
-                  
+
     describe package('serverbackup-agent') do
       it { should be_installed }
     end
@@ -19,5 +19,52 @@ describe 'Applying the r1soft::agent class' do
     describe kernel_module('hcpdriver') do
       it { should be_loaded }
     end
+
+
+    describe 'running r1soft::agent with two keys ensured present' do
+      it 'should work with no errors' do
+        pp = <<-EOS
+         class {'::r1soft::agent':
+           keys => {'198.51.100.2' => {ensure => 'present', key => 'foo',},
+                    '198.51.100.3' => {ensure => 'present', key => 'bar',}}
+         }
+      EOS
+
+        # Run it twice and test for idempotency
+        apply_manifest(pp, :catch_failures => true)
+        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+      end
+
+      describe file('/usr/sbin/r1soft/conf/server.allow/198.51.100.2') do
+        its(:content) { should match /foo/ }
+      end
+      describe file('/usr/sbin/r1soft/conf/server.allow/198.51.100.3') do
+        its(:content) { should match /bar/ }
+      end
+    end
+
+    describe 'running r1soft::agent with one key ensure present and one key ensure absent' do
+      it 'should work with no errors' do
+        pp = <<-EOS
+         class {'::r1soft::agent':
+           keys => {'198.51.100.2' => {ensure => 'present', key => 'foo',},
+                    '198.51.100.3' => {ensure => 'absent'}}
+         }
+      EOS
+
+        # Run it twice and test for idempotency
+        apply_manifest(pp, :catch_failures => true)
+        expect(apply_manifest(pp, :catch_failures => true).exit_code).to be_zero
+      end
+
+      describe file('/usr/sbin/r1soft/conf/server.allow/198.51.100.2') do
+        its(:content) { should match /foo/ }
+      end
+      describe file('/usr/sbin/r1soft/conf/server.allow/198.51.100.3') do
+        it { should_not exist }
+      end
+    end
+
+
   end
 end
