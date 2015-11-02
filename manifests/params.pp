60cf3661c17e6418a739473ad6f78b50c42487a6
Mark McKinstry <mmckinst@nexcess.net>
2015-11-02 10:54:21 -0500

    accommodate PAE and xen kernels for el5

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,24 @@ class r1soft::params {
   $cdp_agent_package_version  = 'present'
   $cdp_agent_package_name     = 'serverbackup-agent'
   $kernel_devel_install       = true
-  $kernel_devel_package_names = "kernel-devel-${::kernelrelease}"
+
+  # RHEL 5 has two special kernel packages, 'xen' which is only for x86_64 and
+  # 'PAE' which is only i386. The kernelrelease output for these looks something
+  # like '2.6.18-371.4.1.el5xen' so we have to use a regex to convert it to the
+  # correct package name
+  #
+  # RHEL 6 did away with all the silliness and made PAE enabled by default and
+  # made a PAE capable processor a requirement for RHEL 6. See
+  # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/6.0_Release_Notes/kernel.html#idp1066960
+  #
+  if ($::operatingsystem in ['CentOS', 'RedHat'] and $::operatingsystemmajorrelease == 5) {
+    $kernel_devel_package_names = $::kernelrelease ? {
+      /(PAE|xen)$/ => regsubst ($::kernelrelease, '(.*)(PAE|xen)', 'kernel-\2-devel-\1'),
+      default      => "kernel-devel-${::kernelrelease}",
+    }
+  } else {
+    $kernel_devel_package_names = "kernel-devel-${::kernelrelease}"
+  }
   $kernel_module_install      = true
   $kernel_module_name         = 'hcpdriver'
   $service_manage             = true
