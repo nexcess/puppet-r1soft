128f15f576334d48c816ad8dd51909d60954d5da
jRome Martin <cumulo741.bitbucket@gmail.com>
2020-02-28 15:13:08 -0500

    2020-02-28T15:13-0500 | Do not notify service on update

--- manifests/agent/hcpdriver.pp
+++ manifests/agent/hcpdriver.pp
@@ -3,11 +3,11 @@ class r1soft::agent::hcpdriver {
   if $r1soft::agent::kmod_manage {
     if $facts['system_uptime']['seconds'] >= $r1soft::agent::delay_factor {
       # check for the hcpdriver_installed fact to prevent weirdness on fresh installs
+      # do not notify service here, since a failure on the command could still provide a return code of 0
       if $facts['hcpdriver_installed'] {
         exec {'update hcpdriver kmod':
           command => "${r1soft::agent::kmod_tool} --get-module --silent",
           creates => $facts['hcpdriver']['kmod_wanted'],
-          notify  => Service[$r1soft::agent::service_name],
         }
 
         ## If the module isn't built, don't even continue
